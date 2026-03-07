daa0e9db53912287f84485f1f7b3b62e10f28358
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2026-03-08 03:59:10 +0530

    Refactor Alien constructor to use prototype for greet method

--- practice question Solutions/Understanding_Constructor/constructorFunction.js
+++ practice question Solutions/Understanding_Constructor/constructorFunction.js
@@ -1,9 +1,13 @@
 function Alien(name, tech){
     this.name = name;
     this.tech = tech;
-    this.greet = function() {
-        console.log(`Hello ${this.name}, You know ${this.tech}`);
-    }
+    // this.greet = function() {
+    //     console.log(`Hello ${this.name}, You know ${this.tech}`);
+    // }
+}
+
+Alien.prototype.greet = function() {
+    console.log(`Hello ${this.name}, You know ${this.tech}`);
 }
 
 
@@ -14,5 +18,8 @@ let alien2 = new Alien("mitchel Jonson", "java");
 console.log(alien1);
 console.log(alien2);
 
+
 alien1.name = "Dj Bravo"
 console.log(alien1)
+
+alien1.greet()
