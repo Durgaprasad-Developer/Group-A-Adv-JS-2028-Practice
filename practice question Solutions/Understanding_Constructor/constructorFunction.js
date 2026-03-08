ffd3c9e2acd43ca9a9a54d700c140e09a26c017b
Durgaprasad-Developer <bdurga.24bcs10012@sst.scaler.com>
2026-03-08 15:02:48 +0530

    Refactor Alien prototype and clean up commented code prototype Chaining

--- practice question Solutions/Understanding_Constructor/constructorFunction.js
+++ practice question Solutions/Understanding_Constructor/constructorFunction.js
@@ -10,13 +10,28 @@ Alien.prototype.greet = function() {
     console.log(`Hello ${this.name}, You know ${this.tech}`);
 }
 
+console.log("Prototype of Alien: ", Alien.prototype)
+
+// function alien(name, tech) {
+// // setDetails : function (name, tech){
+// this.name = name;
+// this.tech = tech;
+// // }
+// }
+
 
 
 let alien1 = new Alien("bravo", "js");
 let alien2 = new Alien("mitchel Jonson", "java");
 
+// let star1 = new alien("mitchel stark", "baller");
+// let star2 = new alien("Mbape", "Striker");
+
 console.log(alien1);
 console.log(alien2);
+
+// console.log(star1);
+// console.log(star2);
 
 
 alien1.name = "Dj Bravo"
